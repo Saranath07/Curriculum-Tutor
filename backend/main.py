94048d29f1ce7e6d900d82df506f32fa1578c12d
Saranath07 <saranathp34@gmail.com>
2023-10-16 17:34:43 +0530

    Basic Backend Changes

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,65 @@
+import os
 from flask import Flask
+from flask_restful import Api
+from application.config import LocalDevelopmentConfig
+from flask_cors import CORS
+from application.database import db
+from flask_jwt_extended import JWTManager
+from application.models import Users
+from application import workers
+from celery.schedules import timedelta
+from flask_sse import sse
 
-app = Flask(__name__)
+app = None
+api = None
+celery = None
+def create_app():
+    
+    app = Flask(__name__)
+    if os.getenv("ENV","development") == "production":
+        raise Exception("Currently no production config is setup.")
+    else:
+        print("Starting Local Development")
+        app.config.from_object(LocalDevelopmentConfig)
+    db.init_app(app)
+    
+    
+    api = Api(app)
+    app.app_context().push()
+    celery = workers.celery
+    celery.conf.update(
+        broker_url = app.config["CELERY_BROKER_URL"],
+        result_backend = app.config["CELERY_RESULT_BACKEND"]
+    )
+    celery.Task = workers.ContextTask
+    app.app_context().push()
+    
+    
+    return app, api, celery
 
-@app.route("/")
-def hello():
-    return "Hello World"
+app, api, celery = create_app()
+
+cors = CORS(app) # Allow cross-origin requests 
+api = Api(app)
+
+jwt = JWTManager(app)
+with app.app_context():
+    db.create_all()
+
+app.register_blueprint(sse, url_prefix='/stream')
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
+    
     app.run(
-        debug = True
+        debug = True,
+        
     )
-
