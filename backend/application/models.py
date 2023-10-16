9e41d1764bee9afb5d95ed12d5711574a21c969d
jandileep <jananidileepan@gmail.com>
2023-10-16 21:00:43 +0530

    Added things into models.py pls checkout

--- backend/application/models.py
+++ backend/application/models.py
@@ -3,11 +3,33 @@ from .database import db
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key=True)
-    stud_name = db.Column(db.String())
-    # std = db.Column(db.Integer())
+    user_name = db.Column(db.String())
     email = db.Column(db.String())
     role = db.Column(db.String())
+    # performance = db.Column(db.String())
+    password = db.Column(db.String())
+    pic = db.Column(db.String())
 
-class questions(db.Model):
-    __tablename__ = 'question'
-    ques_id = db.Column
+class Questions(db.Model):
+    __tablename__ = 'questions'
+    id = db.Column(db.Integer(),primary_key=True)
+    question = db.Column(db.String())
+    topic = db.Column(db.String())
+    category = db.Column(db.String())
+    options = db.Column(db.String())
+    correct_options = db.Column(db.String())
+    rating = db.Column(db.Integer())
+
+class Performance(db.Model):
+    __tablename__ = 'performance'
+    user_id = db.Column(db.Integer, db.ForeignKey(Users.id, ondelete='CASCADE'))
+    easy = db.Column(db.Integer())
+    medium = db.Column(db.Integer())
+    hard = db.Column(db.Integer())
+    performance = db.Column(db.String())
+
+
+
+
+
+
