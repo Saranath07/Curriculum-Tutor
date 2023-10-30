b5ddde21c669d1eed6de852e895a4c26ac4cbd45
jandileep <jananidileepan@gmail.com>
2023-10-30 15:38:06 +0530

    ChartJs Completed

--- frontend/components/NumberLine.vue
+++ frontend/components/NumberLine.vue
@@ -1,37 +1,32 @@
 <template>
   <div>
     <h1>Number Line Chart</h1>
-    
+
     <div>
       <label for="numerator">Numerator:</label>
-      <input type="number" v-model="numerator" min="0" step="1">
+      <input type="number" v-model="numerator" min="0" step="10">
       <label for="denominator">Denominator:</label>
-      <input type="number" v-model="denominator" min="1" step="1">
+      <input type="number" v-model="denominator" min="1" step="10">
     </div>
 
-    <canvas ref="numberLineCanvas" width="400" height="100"></canvas>
+    <canvas id="number-line-chart" ref="numberLineCanvas" width="400" height="100"></canvas>
   </div>
 </template>
 
-<script>
-import Chart from 'chart.js';
+<script setup lang="ts">
+import { ref, onMounted, watchEffect } from 'vue';
+import {Chart, LinearScale, LineController, PointElement, LineElement} from 'chart.js';
 
-export default {
-  data() {
-    return {
-      numerator: 0,
-      denominator: 10,
-      numberLineChart: null,
-    };
-  },
-  watch: {
-    numerator: 'updateNumberLine',
-    denominator: 'updateNumberLine',
-  },
-  methods: {
-    updateNumberLine() {
-      const selectedNumerator = parseInt(this.numerator);
-      const selectedDenominator = parseInt(this.denominator);
+    Chart.register(LinearScale, LineController, PointElement, LineElement)
+    const numerator = ref(0);
+    const denominator = ref(10);
+    const numberLineCanvas = ref<HTMLCanvasElement | null>(null)
+
+    let numberLineChart: Chart | null = null
+
+    const updateNumberLine = () => {
+      const selectedNumerator = numerator.value;
+      const selectedDenominator = denominator.value;
 
       const dataPoints = [];
       const scale = Math.max(selectedNumerator, selectedDenominator);
@@ -42,21 +37,39 @@ export default {
         dataPoints.push({ x, y: 0, highlighted: i === selectedNumerator || i === selectedDenominator });
       }
 
-      if (this.numberLineChart) {
-        this.numberLineChart.data.datasets[0].data = dataPoints;
-        this.numberLineChart.options.scales.x.max = maxScale;
-        this.numberLineChart.update();
-      } else {
-        this.numberLineChart = new Chart(this.$refs.numberLineCanvas, {
+      if (numberLineChart) {
+        numberLineChart.data.datasets[0].data = dataPoints;
+        //@ts-ignore x
+        numberLineChart.options.scales.x.max = maxScale;
+        numberLineChart.update();
+      } 
+    };
+
+    onMounted(() => {
+      nextTick(() => {
+        const selectedNumerator = numerator.value;
+      const selectedDenominator = denominator.value;
+
+      const dataPoints = [];
+      const scale = Math.max(selectedNumerator, selectedDenominator);
+      const maxScale = scale + 1;
+
+      for (let i = 0; i <= maxScale; i++) {
+        const x = i;
+        dataPoints.push({ x, y: 0, highlighted: i === selectedNumerator || i === selectedDenominator });
+      }
+        numberLineChart = new Chart(numberLineCanvas.value as HTMLCanvasElement, {
           type: 'line',
           data: {
-            datasets: [{
-              data: dataPoints,
-              borderColor: 'black',
-              borderWidth: 2,
-              pointRadius: 5,
-              pointBackgroundColor: (ctx) => ctx.raw.highlighted ? 'red' : 'blue',
-            }]
+            datasets: [
+              {
+                data: dataPoints,
+                borderColor: 'black',
+                borderWidth: 2,
+                pointRadius: 5,
+                pointBackgroundColor: (ctx) => ctx.raw.highlighted ? 'red' : 'blue',
+              },
+            ],
           },
           options: {
             scales: {
@@ -68,21 +81,26 @@ export default {
               },
               y: {
                 display: false,
-              }
-            }
-          }
+              },
+            },
+          },
         });
-      }
-    }
-  },
-  mounted() {
-    this.updateNumberLine();
-  },
-};
+      })
+    });
+
+    // Watch for changes in numerator and denominator
+    watchEffect(updateNumberLine);
+
 </script>
 
 <style>
+/* Add your CSS styles here */
 #number-line-chart {
   margin-top: 20px;
+}
+
+.highlighted-point {
+  background-color: yellow;
+  radius: 8;
 }
 </style>
--- frontend/pages/trial.vue
+++ frontend/pages/trial.vue
@@ -1,53 +1,106 @@
-<script setup lang="ts">
-import { RadioGroupIndicator, RadioGroupItem, RadioGroupRoot } from 'radix-vue'
-import { ref } from 'vue'
+<template>
+  <div>
+    <h1>Number Line Chart</h1>
 
-const radioStateSingle = ref('default')
+    <div>
+      <label for="numerator">Numerator:</label>
+      <input type="number" v-model="numerator" min="0" step="10">
+      <label for="denominator">Denominator:</label>
+      <input type="number" v-model="denominator" min="1" step="10">
+    </div>
+
+    <canvas id="number-line-chart" ref="numberLineCanvas" width="400" height="100"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, watchEffect } from 'vue';
+import {Chart, LinearScale, LineController, PointElement, LineElement} from 'chart.js';
+
+    Chart.register(LinearScale, LineController, PointElement, LineElement)
+    const numerator = ref(0);
+    const denominator = ref(10);
+    const numberLineCanvas = ref<HTMLCanvasElement | null>(null)
+
+    let numberLineChart: Chart | null = null
+
+    const updateNumberLine = () => {
+      const selectedNumerator = numerator.value;
+      const selectedDenominator = denominator.value;
+
+      const dataPoints = [];
+      const scale = Math.max(selectedNumerator, selectedDenominator);
+      const maxScale = scale + 1;
+
+      for (let i = 0; i <= maxScale; i++) {
+        const x = i;
+        dataPoints.push({ x, y: 0, highlighted: i === selectedNumerator || i === selectedDenominator });
+      }
+
+      if (numberLineChart) {
+        numberLineChart.data.datasets[0].data = dataPoints;
+        //@ts-ignore x
+        numberLineChart.options.scales.x.max = maxScale;
+        numberLineChart.update();
+      } 
+    };
+
+    onMounted(() => {
+      nextTick(() => {
+        const selectedNumerator = numerator.value;
+      const selectedDenominator = denominator.value;
+
+      const dataPoints = [];
+      const scale = Math.max(selectedNumerator, selectedDenominator);
+      const maxScale = scale + 1;
+
+      for (let i = 0; i <= maxScale; i++) {
+        const x = i;
+        dataPoints.push({ x, y: 0, highlighted: i === selectedNumerator || i === selectedDenominator });
+      }
+        numberLineChart = new Chart(numberLineCanvas.value as HTMLCanvasElement, {
+          type: 'line',
+          data: {
+            datasets: [
+              {
+                data: dataPoints,
+                borderColor: 'black',
+                borderWidth: 2,
+                pointRadius: 5,
+                pointBackgroundColor: (ctx) => ctx.raw.highlighted ? 'red' : 'blue',
+              },
+            ],
+          },
+          options: {
+            scales: {
+              x: {
+                type: 'linear',
+                position: 'bottom',
+                min: 0,
+                max: maxScale,
+              },
+              y: {
+                display: false,
+              },
+            },
+          },
+        });
+      })
+    });
+
+    // Watch for changes in numerator and denominator
+    watchEffect(updateNumberLine);
+
 </script>
 
-<template>
-  <RadioGroupRoot
-    v-model="radioStateSingle" class="flex flex-col gap-2.5" default-value="default"
-    aria-label="View density"
-  >
-    <div class="flex items-center">
-      <RadioGroupItem
-        id="r1"
-        class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7 hover:bg-green3 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default" value="default"
-      >
-        <RadioGroupIndicator
-          class="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-grass11"
-        />
-      </RadioGroupItem>
-      <label class="text-white text-[15px] leading-none pl-[15px]" for="r1">
-        Default
-      </label>
-    </div>
-    <div class="flex items-center">
-      <RadioGroupItem
-        id="r2"
-        class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7 hover:bg-green3 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default" value="comfortable"
-      >
-        <RadioGroupIndicator
-          class="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-grass11"
-        />
-      </RadioGroupItem>
-      <label class="text-white text-[15px] leading-none pl-[15px]" for="r2">
-        Comfortable
-      </label>
-    </div>
-    <div class="flex items-center">
-      <RadioGroupItem
-        id="r3"
-        class="bg-white w-[25px] h-[25px] rounded-full shadow-[0_2px_10px] shadow-blackA7 hover:bg-green3 focus:shadow-[0_0_0_2px] focus:shadow-black outline-none cursor-default" value="compact"
-      >
-        <RadioGroupIndicator
-          class="flex items-center justify-center w-full h-full relative after:content-[''] after:block after:w-[11px] after:h-[11px] after:rounded-[50%] after:bg-grass11"
-        />
-      </RadioGroupItem>
-      <label class="ml-5" for="r3">
-        Compact
-      </label>
-    </div>
-  </RadioGroupRoot>
-</template>
+<style>
+/* Add your CSS styles here */
+#number-line-chart {
+  margin-top: 20px;
+}
+
+.highlighted-point {
+  background-color: yellow;
+  radius: 8;
+}
+</style>
