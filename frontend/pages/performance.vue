2d96e3cd817d3f3ebdfbb95d80a09a677aa2d631
Saranath07 <saranathp34@gmail.com>
2023-12-03 19:42:49 +0530

    Changes made in performance page and Home page

--- frontend/pages/performance.vue
+++ frontend/pages/performance.vue
@@ -1,5 +1,118 @@
 <template>
-    <div>
-        Performance Page
+    <div class="page-container">
+      <h1 style="text-align: center; font-weight: bold;">Your Performance</h1>
+      <table class="performance-table">
+        <thead>
+          <tr>
+            <th>Topic Name</th>
+            <th>Number of Questions Solved</th>
+            <th>Mastered</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(topic, index) in performanceData" :key="index">
+            <td>{{ topic.name }}</td>
+            <td>{{ topic.questionsSolved }}</td>
+            <td :class="{ mastered: topic.mastered }">{{ topic.mastered ? 'Yes' : 'No' }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="bar-chart-container">
+        <canvas id="barChart" width="10px" height="20px"></canvas>
+      </div>
     </div>
-</template>
+    
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from "vue";
+  import { Chart, registerables } from 'chart.js';
+  Chart.register(...registerables);
+  
+  const performanceData = ref([
+    { name: 'Topic 1', questionsSolved: 25, mastered: true },
+    { name: 'Topic 2', questionsSolved: 20, mastered: false },
+    { name: 'Topic 3', questionsSolved: 18, mastered: true },
+    // Add more topics as needed
+  ]);
+  
+  let barChart;
+  
+  onMounted(() => {
+    createBarChart();
+  });
+  
+  function createBarChart() {
+    const canvas = document.getElementById('barChart');
+    const ctx = canvas.getContext("2d");
+  
+    const chartData = {
+      labels: performanceData.value.map(topic => topic.name),
+      datasets: [{
+        label: 'Number of Questions Solved',
+        data: performanceData.value.map(topic => topic.questionsSolved),
+        backgroundColor: performanceData.value.map(topic => topic.mastered ? 'rgba(0, 255, 0, 0.5)' : 'rgba(255, 0, 0, 0.5)'),
+        borderColor: performanceData.value.map(topic => topic.mastered ? 'rgba(0, 255, 0, 1)' : 'rgba(255, 0, 0, 1)'),
+        borderWidth: 1
+      }]
+    };
+  
+    const chartOptions = {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    };
+  
+    barChart = new Chart(ctx, {
+      type: 'bar',
+      data: chartData,
+      options: chartOptions
+    });
+  }
+  </script>
+
+  <style scoped>
+ .performance-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+.performance-table th, .performance-table td {
+  border: 1px solid #ddd;
+  padding: 10px;
+  text-align: left;
+}
+
+.performance-table th {
+  background-color: #f2f2f2;
+}
+
+.performance-table td:nth-child(3).mastered {
+  color: green;     /* Color for mastered topics */
+  font-weight: bold;
+}
+
+.performance-table td:nth-child(3):not(.mastered) {
+  color: red;       /* Color for non-mastered topics */
+  font-weight: bold;
+}
+
+  .page-container{
+    margin-top: 10px;
+    margin-bottom: 50px;
+    margin-left: 30px;
+    margin-right: 30px;
+}
+.bar-chart-container {
+  width: 40%; /* Adjust the width as needed */
+  margin-top: 50px;
+  margin-left: 450px;
+  height: 50%;
+  text-align: center;
+  justify-content: center;
+}
+  /* Add more styles as needed */
+  </style>
--- frontend/pages/questions/[...id].vue
+++ frontend/pages/questions/[...id].vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
+    <div class="page-container">
       <article class="prose prose-md mx-auto flex flex-col gap-4">
         <MDC :value="question" tag="article" />
-  
+        <img :src="imgPath" class="question-image" alt="Question Picture" >
         <div class="flex gap-4" v-for="option in options">
           <input type="radio" :id="option" name="option" :value="option">
           <label :for="option">
@@ -12,22 +12,32 @@
   
         <div>
           <button
+            class="px-2 py-4 w-24 transition duration-500 ease-in-out transform hover:-translate-y-1 bg-yellow-600 text-white uppercase text-xs rounded-md" 
+            @click="previousPage">
+           Previous
+
+          </button>
+          
+          <button
+                    class="px-2 py-4 w-24 transition duration-500 ease-in-out transform hover:-translate-y-1 bg-green-600 text-white uppercase text-xs rounded-md"
+                    style="margin-left: 50px;" @click="nextpage">Next</button>
+
+          <button
             class="px-2 py-4 w-24 transition duration-500 ease-in-out transform hover:-translate-y-1 bg-rose-600 text-white uppercase text-xs rounded-md"
-          >
+            style="margin-left: 300px;">
             Submit
           </button>
   
-          <button
+          <!-- <button
             class="px-2 py-4 w-24 transition duration-500 ease-in-out transform hover:-translate-y-1 bg-blue-600 text-white uppercase text-xs rounded-md"
             @click="showNumberLine" style="margin-left: 50px;"
           >
             Number Line
 
-          </button>
+          </button> -->
+          
 
-          <button
-                    class="px-2 py-4 w-24 transition duration-500 ease-in-out transform hover:-translate-y-1 bg-green-600 text-white uppercase text-xs rounded-md"
-                    style="margin-left: 300px;">Next</button>
+         
             
         </div>
       </article>
@@ -43,12 +53,17 @@
   
   <script setup>
   import { ref } from "vue";
-  import { useRoute } from "vue-router";
+  import { useRoute, useRouter } from "vue-router";
   
   const question = ref("");
   const route = useRoute();
   const q_no = ref(route.params.id);
   const show = ref(false);
+  const router = useRouter()
+  const imgPath = ref("")
+  imgPath.value = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQttuTvK6dGgaC7yHWk1bPmCTtuo-6-b0kB-SPRz1EFPUFhZaqZRrV_LJNKaf8c8-cnxY&usqp=CAU"
+
+  console.log("Question:", q_no.value);
   
   function showNumberLine() {
     show.value = true;
@@ -57,11 +72,30 @@
   function closeNumberLine() {
     show.value = false;
   }
+
+  function nextpage(){
+    
+    const newUrl = `/questions/${Number(q_no.value) + 1}`;
+    console.log(newUrl)
+    router.push(newUrl);
+  }
+  function previousPage(){
+    const qNo = Number(q_no.value) - 1;
+    if (qNo > 0){
+      const newUrl = `/questions/${qNo}`;
+      console.log(newUrl)
+      router.push(newUrl);
+    }else{
+      console.log('No questions behind')
+      alert('No questions behind');
+    }
+  }
   
-  question.value = `# Question ${q_no.value} 
-  What is the decimal value of $0.5?$`;
+  question.value = `# Question ${q_no.value} \
   
-  const options = ref(["$\\dfrac{1}{2}$", "$\\dfrac{1}{4}$"]);
+  What does this fraction represent?`;
+  
+  const options = ref(["$\\dfrac{1}{2}$", "$\\dfrac{1}{4}$", "$\\dfrac{2}{5}$"]);
   </script>
   
   <style scoped>
@@ -88,5 +122,17 @@
     padding: 5px 10px;
     cursor: pointer;
   }
+  .question-image {
+  max-width: 40%; /* Ensure the image doesn't exceed its container */
+  height: 200px;    /* Maintain the aspect ratio of the image */
+  border: 1px solid #ddd; /* Add a border for better visibility */
+  border-radius: 5px; /* Add border radius for rounded corners */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Add a subtle box shadow */
+  margin-bottom: 10px; /* Add some space below the image */
+}
+.page-container{
+  margin-top: 10px;
+  margin-bottom: 50px;
+}
   </style>
   
